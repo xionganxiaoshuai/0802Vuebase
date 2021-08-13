2af80ce6eeaacfb83c2df468e9cc66f9d554532a
i love you mi <email youchuangjiankang>
2021-08-13 11:14:39 +0800

    axios

---
+++ src-axios/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+  <div class="container">
+    <Header></Header>
+    <Main></Main>
+    
+  </div>
+</div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+  export default {
+  name: 'App',
+  components:{
+    Header,
+    Main
+  },
+
+  data() {
+    return {
+            
+    };
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
